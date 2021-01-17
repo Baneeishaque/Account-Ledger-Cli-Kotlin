2d3ca9ee6032b06aa15c424295772f83d31e6b20
Banee Ishaque K <baneeishaque@hotmail.com>
2021-01-17 15:30:01 +0000

    App.kt Fix

--- cli-app/src/main/kotlin/accountLedgerCli/cli/App.kt
+++ cli-app/src/main/kotlin/accountLedgerCli/cli/App.kt
@@ -50,8 +50,8 @@ private val accountsResponseResult = AccountsResponse(1, listOf(AccountUtils.get
 
 fun main(args: Array<String>) {
 
-    readCsv()
-    return
+    // readCsv()
+    // return
 
     val parser = ArgParser(programName = "${App.appName}:: ${App.version}")
     val version by parser.option(type = ArgType.Boolean, shortName = "V", description = "Version").default(value = false)
