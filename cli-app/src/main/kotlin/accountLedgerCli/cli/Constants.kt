94d22e72883d8047d5f6b4ba56a6c0740e348099
Banee Ishaque K <baneeishaque@gmail.com>
2022-07-03 14:46:17 +0530

    Changes in Frequent Accounts

--- cli-app/src/main/kotlin/accountLedgerCli/cli/Constants.kt
+++ cli-app/src/main/kotlin/accountLedgerCli/cli/Constants.kt
@@ -1,11 +1,15 @@
 package accountLedgerCli.cli
 
 internal const val baneeWalletAccountId = 6
+
 internal const val baneeBankAccountId = 11
 internal const val baneeBankAccountName = "Punjab National Bank, Tirur"
-internal const val baneeFrequent1AccountId = 688
-internal const val baneeFrequent1AccountName = "Hisham Banee Ishaque K Brother"
-internal const val baneeFrequent2AccountId = 38
-internal const val baneeFrequent2AccountName = "Ismail K Father Banee Ishaque K"
+
+internal const val baneeFrequent1AccountId = 38
+internal const val baneeFrequent1AccountName = "Ismail K Father Banee Ishaque K"
+
+internal const val baneeFrequent2AccountId = 3111
+internal const val baneeFrequent2AccountName = "Account Clearances"
+
 internal const val baneeFrequent3AccountId = 367
 internal const val baneeFrequent3AccountName = "Account Shortages"
