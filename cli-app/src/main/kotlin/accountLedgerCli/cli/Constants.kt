088478770bbeb0ea41866af345bc0e786ac5cb8a
Banee Ishaque K <baneeishaque@gmail.com>
2022-07-13 17:49:39 +0530

    Clean

--- cli-app/src/main/kotlin/accountLedgerCli/cli/Constants.kt
+++ cli-app/src/main/kotlin/accountLedgerCli/cli/Constants.kt
@@ -1,18 +1,7 @@
 package accountLedgerCli.cli
 
 internal const val walletAccountId = 6
-
 internal const val bankAccountId = 11
-
 internal const val frequent1AccountId = 38
-
 internal const val frequent2AccountId = 3111
-
 internal const val frequent3AccountId = 367
-
-//class EnvironmentFileFields {
-//    companion object {
-//        internal const val username: String = "USER_NAME"
-//        internal const val password: String = "PASSWORD"
-//    }
-//}
