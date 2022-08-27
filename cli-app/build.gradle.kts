846427f1314c09154bb51297e0d6fd9ff7311ee4
Banee Ishaque K <Baneeishaque@gmail.com>
2022-08-27 19:14:42 +0000

    Gradle 7.0

--- cli-app/build.gradle.kts
+++ cli-app/build.gradle.kts
@@ -41,9 +41,13 @@ application {
 }
 
 val jar by tasks.getting(Jar::class) {
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
     manifest {
         attributes["Main-Class"] = "accountLedgerCli.cli.App"
     }
+    
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }) {
         exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
     }
