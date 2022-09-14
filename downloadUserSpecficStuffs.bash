0d3b0b0980c5562125423709ec7fe14877d7f563
Banee Ishaque K <baneeishaque@gmail.com>
2022-09-14 19:15:57 +0530

    customisation for never install

--- downloadUserSpecficStuffs.bash
+++ downloadUserSpecficStuffs.bash
@@ -1,1 +1,2 @@
 cd /workspace && git clone https://github.com/Baneeishaque/configurations-private.git && cd Account-Ledger-Cli-Kotlin-Gradle && ln -s /workspace/configurations-private/AccountLedger/.env && ln -s /workspace/configurations-private/AccountLedger/frequencyOfAccounts.json && cd api && ln -s /workspace/configurations-private/AccountLedger/http-client.env.json
+#ln -s ../configurations-private/Accoun/tLedger/.env && ln -s ../configurations-private/AccountLedger/frequencyOfAccounts.json && cd api && ln -s ../configurations-private/AccountLedger/http-client.env.json
