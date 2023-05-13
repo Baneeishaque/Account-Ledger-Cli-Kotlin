bc428a82137e741eaa4b60e72e22002720fcf831
Banee Ishaque K <baneeishaque@gmail.com>
2023-05-13 13:50:35 +0530

    Typo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,6 @@
 rootProject.name = "accountLedgerCli"
 
+include(":cli-app")
+include(":account-ledger-lib:account-ledger-lib")
 include("common-lib:common-lib")
 project(":common-lib:common-lib").projectDir = file("account-ledger-lib/common-lib/common-lib")
-
-include(":account-ledger-lib:account-ledger-lib")
-
-include(":cli-app")
