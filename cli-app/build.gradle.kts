911bf3dd77f6aee6eedcedb9c32bbcee3896f15c
Banee Ishaque K <baneeishaque@gmail.com>
2023-05-28 01:45:13 +0530

    Kotlin 1.9.20-dev-947

--- cli-app/build.gradle.kts
+++ cli-app/build.gradle.kts
@@ -2,8 +2,10 @@
 val ktorVersion: String = "2.1.2"
 
 plugins {
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.serialization") version "1.8.21"
+
+    val kotlinVersion = "1.9.20-dev-947"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
 
     application
     jacoco
