062a7de1e4d2bee51b985c085f78b408282188cf
Banee Ishaque K <baneeishaque@gmail.com>
2023-11-30 21:29:35 +0530

    Kotlin 2.0.0-Beta1-16

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
 
     // TODO : Use BoM
-    // val kotlinVersion = "1.9.20-mercury-653"
-    val kotlinVersion = "1.9.20-station-601"
+    val kotlinVersion = "2.0.0-Beta1-16"
     kotlin("jvm") version kotlinVersion apply false
+    kotlin("multiplatform") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
 }
