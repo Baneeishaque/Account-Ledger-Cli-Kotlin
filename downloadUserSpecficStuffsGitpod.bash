ea63d065e87cd719303a93ab99c8e00c5baf09c2
Banee Ishaque K <Baneeishaque@gmail.com>
2023-12-08 18:03:15 +0530

    downloadUserSpecficStuffsGitpod.bash

--- downloadUserSpecficStuffsGitpod.bash
+++ downloadUserSpecficStuffsGitpod.bash
@@ -1,1 +1,9 @@
-cd /workspace && git clone https://github.com/Baneeishaque/configurations-private.git && cd Account-Ledger-Cli-Kotlin-Gradle && ln -s /workspace/configurations-private/AccountLedger/.env && ln -s /workspace/configurations-private/AccountLedger/frequencyOfAccounts.json && cd api && ln -s /workspace/configurations-private/AccountLedger/http-client.env.json
+cd /workspace
+if [ ! -d configurations-private ];then
+    git clone https://github.com/Baneeishaque/configurations-private.git
+else
+    cd configurations-private
+    git pull
+    cd ..
+fi
+cd Account-Ledger-Cli-Kotlin-Gradle && ln -s /workspace/configurations-private/AccountLedger/.env && ln -s /workspace/configurations-private/AccountLedger/frequencyOfAccounts.json && cd api && ln -s /workspace/configurations-private/AccountLedger/http-client.env.json
