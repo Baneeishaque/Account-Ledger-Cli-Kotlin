207a3b5a74a1e0babfe8cf8338aa6572c8b86eca
Banee Ishaque K <baneeishaque@gmail.com>
2023-12-14 00:52:30 +0530

    Update kotlin monorepo to v2.0.0-test-888

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 
     // TODO : Use BoM
-    val kotlinVersion = "2.0.0-Beta1-16"
+    val kotlinVersion = "2.0.0-test-888"
     kotlin("jvm") version kotlinVersion apply false
     kotlin("multiplatform") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
