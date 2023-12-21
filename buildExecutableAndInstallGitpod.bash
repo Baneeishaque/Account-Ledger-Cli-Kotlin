8b8371331ae49a60ed04c3e472c3c9c13b3c000c
Banee Ishaque K <Baneeishaque@gmail.com>
2023-12-21 17:11:03 +0530

    buildExecutableAndInstallGitpod.bash

--- buildExecutableAndInstallGitpod.bash
+++ buildExecutableAndInstallGitpod.bash
@@ -7,5 +7,14 @@ mv cli-app Account-Ledger-Cli
 chmod a+x Account-Ledger-Cli
 mv cli-app.bat Account-Ledger-Cli.bat
 cd ../..
+rm -r /workspace/Account-Ledger-Cli
 mv Account-Ledger-Cli /workspace/
 cd /workspace
+if [ ! -d configurations-private ];then
+    git clone https://github.com/Baneeishaque/configurations-private.git
+else
+    cd configurations-private
+    git pull
+    cd ..
+fi
+cd Account-Ledger-Cli/bin && ln -s /workspace/configurations-private/AccountLedger/.env && ln -s /workspace/configurations-private/AccountLedger/frequencyOfAccounts.json && ln -s /workspace/configurations-private/AccountLedger/relationOfAccounts.json
