ffcb04de97268efbef9d7a25867bf9051b354957
Banee Ishaque K <baneeishaque@gmail.com>
2025-07-25 21:05:42 +0530

    fix(download script): Remove old symbolic links before creating new ones

--- downloadUserSpecficStuffsGitpodAccountLedgerSource.bash
+++ downloadUserSpecficStuffsGitpodAccountLedgerSource.bash
@@ -7,4 +7,4 @@ else
     git pull
     cd ..
 fi
-cd Account-Ledger-Cli-Kotlin && ln -s /workspace/configurations-private/AccountLedger/.env .env && ln -s /workspace/configurations-private/AccountLedger/frequencyOfAccounts.json frequencyOfAccounts.json && ln -s /workspace/configurations-private/AccountLedger/relationOfAccounts.json relationOfAccounts.json && cd api && ln -s /workspace/configurations-private/AccountLedger/http-client.env.json http-client.env.json
+cd Account-Ledger-Cli-Kotlin && rm .env && rm frequencyOfAccounts.json && rm relationOfAccounts.json && rm api/http-client.env.json && ln -s /workspace/configurations-private/AccountLedger/.env .env && ln -s /workspace/configurations-private/AccountLedger/frequencyOfAccounts.json frequencyOfAccounts.json && ln -s /workspace/configurations-private/AccountLedger/relationOfAccounts.json relationOfAccounts.json && cd api && ln -s /workspace/configurations-private/AccountLedger/http-client.env.json http-client.env.json
